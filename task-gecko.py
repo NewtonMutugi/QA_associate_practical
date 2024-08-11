d37101e25117a3f68623b4b59a54106424aea29b
NewtonMutugi <newtonmut197@gmail.com>
2024-08-11 13:57:43 +0300

    feat: Added task-gecko.py

---
+++ task-gecko.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+from dotenv import load_dotenv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+load_dotenv()
+
+# Define variables
+username = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
+url = os.getenv("URL")
+
+print(f"username: {username}\nPassword: {password}")
+
+options = webdriver.FirefoxOptions()
+options.binary_location = os.getenv("FIREFOX_LOCATION")
+options.add_argument("--no-sand-box")
+options.add_argument("--start-maximised")
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+
+profile_path = os.getenv("FIREFOX_PROFILE_PATH")
+profile = webdriver.FirefoxProfile(profile_path)
+options.profile = profile
+
+# Connect to KenyaEMR
+driver = driver = webdriver.Firefox(options)
+driver.get(url)
+driver.implicitly_wait(5)
+
+# Navigate to username field
+username_field = driver.find_element(By.ID, "username")
+username_field.click()
+username_field.send_keys(username)
+button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+button.click()
+
+# Navigate to password field
+password_field = driver.find_element(By.ID, "password")
+password_field.click()
+password_field.send_keys(password)
+login_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+login_btn.click()
+driver.implicitly_wait(5)
+
+try:
+    xpath = '//*[@id="single-spa-application:@kenyaemr/esm-login-app-page-0"]/div/div[1]/div[1]/div/div/div/div[2]'
+    error_msg = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, xpath))).get_attribute("innerText")
+    error_msg = error_msg.strip()
+
+    # Confirm whether error message text is for invalid username or password
+    assert error_msg == "Invalid username or password"
+    print(error_msg)
+except (NoSuchElementException, TimeoutException):
+    print("Login successful")
+except AssertionError:
+    print(f"Unexpected output: {error_msg}")
+
+
+driver.quit()
